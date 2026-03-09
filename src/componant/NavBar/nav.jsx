32fa504350fc44ad73ee6b1bee6a428acfbd0bb6
me2sumanislam <me2sumanislam247@gmail.com>
2026-03-09 16:07:02 +0600

    toggle nav md flex hobe abng small hidden hobe

--- src/componant/NavBar/nav.jsx
+++ src/componant/NavBar/nav.jsx
@@ -1,6 +1,6 @@
- import React from 'react'
+ import React, { useState } from 'react'
  import Link from './Link';
-import { Menu } from 'lucide-react';
+import { Menu, X } from 'lucide-react';
  const navigationData = [
   { id: 1, name: "Home", path: "/" },
   { id: 2, name: "About", path: "/about" },
@@ -10,13 +10,32 @@ import { Menu } from 'lucide-react';
 ];
 
   function Navbar() {
+  const [open,setOpen] = useState(false);
+ const link = navigationData.map(route => <Link key={route.id} route ={route}></Link>)
+
+
    return (
      <nav className='flex justify-between mx-7'>
-             <Menu></Menu>
-        <h2>MyNavbar</h2>
-       <ul className='flex'>
-        {
-            navigationData.map(route => <Link key={route.id} route ={route}></Link>)
+        {/* span ar modhe toggle korte chachi span ar modhe akta array function dilam  */}
+       <span className='flex' onClick={() => setOpen(!open) }>
+         { open ?
+        // x lucide icone crose(x) anar jonno x use kora hoyeche
+         <X className='md:hidden'></X> :
+         <Menu className='md:hidden'></Menu>
+
+         }
+         <ul className='md:hidden'>
+            {/* icone toggle  jonno aikahe ar akta same nav dilam  */}
+            {link}
+         </ul>
+      
+        <h2 className='ml-4'>MyNavbar</h2>
+       </span>
+       <ul className='md:flex hidden'>
+        {/* md:flex hidden jokhon dibo  mediam screen flex hobe and small hidden thakbe */}
+        {   //ai khan theke map ta cut kore akta variable banabo Link name ar poriborte link bosabo
+            // navigationData.map(route => <Link key={route.id} route ={route}></Link>)
+            link
         }
        </ul>
 
